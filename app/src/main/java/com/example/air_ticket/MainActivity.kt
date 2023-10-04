9b4f2107271516ae54573cd4ad09ecf348b2cca1
Lutfullayev <ilhomlutfullayev8@gmail.com>
2023-10-04 23:32:15 +0500

    v2

--- app/src/main/java/com/example/air_ticket/MainActivity.kt
+++ app/src/main/java/com/example/air_ticket/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.air_ticket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.air_ticket.databinding.ActivityMainBinding
+import com.google.android.material.progressindicator.BaseProgressIndicator
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.Button.setOnClickListener() {
+            val from = binding.range.values[0]
+            val to = binding.range.values[1]
+
+            if (binding.chec.isChecked) {
+                val randomnumber = Random.nextInt(from.toInt(), to.toInt())
+                binding.textview.text = "generate : $randomnumber"
+
+            } else {
+
+                val randomnumber = Random.nextDouble(from.toDouble(), to.toDouble())
+                binding.textview.text = "generate : $randomnumber"
+            }
+
+        }
     }
 }
